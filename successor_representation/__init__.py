bb2b9d8831782af63260e8a6b239349df5ccc7ee
Tom <tmssmith@gmail.com>
2024-04-02 17:39:53 +0100

    Import fewer objects

--- successor_representation/__init__.py
+++ successor_representation/__init__.py
@@ -1,3 +1,2 @@
-from successor_representation.successor import Successor, TabularSR, TabularSF
-from successor_representation.feature_extractors import FE, OHE, StateFeatures
-from successor_representation.function_approximators import FA, Table
+from successor_representation.successor import Successor, TabularSR, TabularSF, DeepSF
+from successor_representation.function_approximators import FFNetwork
